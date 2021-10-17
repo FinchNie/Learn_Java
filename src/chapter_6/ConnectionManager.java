09be37c356411c71f4177fa09c90bd61f715af31
FinchNie <2501407942@qq.com>
2021-10-17 22:02:44 +0800

    ch6_ex8

--- src/chapter_6/ConnectionManager.java
+++ src/chapter_6/ConnectionManager.java
@@ -1,5 +1,32 @@
 package chapter_6;
 
-public class ConnectionManager {
+class Connection { 
+	 private static int counter = 0; 
+	 private int id = counter++; 
+	 Connection() {} 
+	 public String toString() { 
+		 return "Connection " + id; 
+	 }
+}
 
+public class ConnectionManager { 
+	 private static Connection[] connections = new Connection[5]; 
+	 private static int counter = 0; 
+	 static { 
+		 for(int i = 0; i < 5; i++) 
+			 connections[i] = new Connection(); 
+	 }
+	 public static Connection getConnection() { 
+		 if(counter < connections.length){
+			 return connections[counter++];
+		 }else{
+			 return null;
+		 }
+	 } 
+	 public static void main(String args[]) { 
+		 for(int i = 0; i < 5; i++){
+			 Connection connection = ConnectionManager.getConnection();
+			 System.out.println(connection);
+		 }
+	 } 
 }
